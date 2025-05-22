406004171235dc33495f17bb413e34fbd9ee4099
sarfarazakram576 <sarfaraz.akram055@gmail.com>
2025-05-22 21:47:50 +0600

    add 2 simple extra sections

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,18 @@
-import React from 'react';
-import Banner from '../../Components/Banner/Banner';
+import React from "react";
+import Banner from "../../Components/Banner/Banner";
+import WhyChooseTaskwave from "../../Components/WhyChooseTaskwave/WhyChooseTaskwave";
+import Faq from "../../Components/Faq/Faq";
+import Featured from "../../Components/Featured/Featured";
 
 const Home = () => {
-    return (
-        <div>
-            <Banner></Banner>
-        </div>
-    );
+  return (
+    <div>
+      <Banner></Banner>
+      <Featured></Featured>
+      <WhyChooseTaskwave></WhyChooseTaskwave>
+      <Faq></Faq>
+    </div>
+  );
 };
 
 export default Home;
