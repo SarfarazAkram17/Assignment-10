b1ec01888916e8972c631e541186fbf776cb5fd5
sarfarazakram576 <sarfaraz.akram055@gmail.com>
2025-05-24 02:09:32 +0600

    complete challange and complete assignment 10

--- src/Pages/TaskDetails/TaskDetails.jsx
+++ src/Pages/TaskDetails/TaskDetails.jsx
@@ -1,41 +1,55 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Link, useLoaderData } from "react-router";
 import { BsArrowLeftCircleFill } from "react-icons/bs";
 import { toast } from "react-toastify";
 import { AuthContext } from "../../Authentication/AuthContext";
 
 const TaskDetails = () => {
-  const {user} = useContext(AuthContext)
+  const { user } = useContext(AuthContext);
   const userEmail = user?.email || user?.providerData?.[0]?.email;
-  const task = useLoaderData();
+  const loadedTask = useLoaderData();
 
+  const [task, setTask] = useState(loadedTask);
 
   const handleBid = (id, email) => {
     fetch(`https://assignment-10-sarfaraz-akram.vercel.app/tasks/${id}`, {
       method: "PATCH",
-       headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ email: email }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email: email }),
     })
       .then((res) => res.json())
       .then((data) => {
         if (data.modifiedCount) {
           toast.success("You Bid successfully for this task");
-        }
-        else{
-          toast.error(data.message)
+
+          setTask((prevTask) => ({
+            ...prevTask,
+            bid: prevTask.bid + 1,
+          }));
+        } else {
+          toast.error(data.message);
         }
       });
   };
+
   return (
     <div className="my-12 px-4">
       <div className="max-w-3xl mx-auto">
-        <Link to={-1}>
-          <BsArrowLeftCircleFill size={22} />
-        </Link>
+        <div className="flex justify-between items-center mb-8">
+          <Link to={-1}>
+            <BsArrowLeftCircleFill size={22} />
+          </Link>
+
+          <h1 className="text-3xl">
+            You bid for <span className="font-black rancho">{task.bid}</span> opportunities.
+          </h1>
+        </div>
+
         <h1 className="text-5xl mb-12 text-center">Task Details</h1>
       </div>
+
       <div className="max-w-3xl mx-auto px-4 md:px-8 py-8 text-center space-y-4 bg-base-200 border-2 border-gray-200 rounded-xl shadow-lg">
         <h1 className="text-4xl md:text-5xl font-bold">{task.title}</h1>
         <p className="text-xs font-semibold">{task.category}</p>
@@ -59,6 +73,7 @@ const TaskDetails = () => {
           <strong>Description: </strong>
           {task.description}
         </p>
+
         <button
           onClick={() => handleBid(task._id, userEmail)}
           className="btn btn-info px-14 my-4 text-2xl rounded-full"
